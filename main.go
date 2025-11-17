8c187cbb5a587e76216892ac5e3870864d2744d5
Daniil Kamyshov <daniilkamyshov2004@yandex.ru>
2025-11-18 00:00:27 +0300

    Change router on chi

--- main.go
+++ main.go
@@ -3,14 +3,16 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 const webDir = ".\\web\\"
 
 func main() {
-	r := http.NewServeMux()
+	r := chi.NewRouter()
 
-	r.Handle("/", http.FileServer(http.Dir(webDir)))
+	r.Handle("/*", http.FileServer(http.Dir(webDir)))
 
 	fmt.Println("RUN")
 	err := http.ListenAndServe(":7540", r)
